365db1e2557e72b38ab0bb53da2ff8541bd56dd6
sajieun <sjieun000721@gmaill.com>
2023-08-25 17:51:27 +0900

    변수와 자료형에 대해서 자세하게 배움

---
+++ FastCampus2/src/CarTest.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class CarTest {
+    public static void main(String[] args) {
+        String model = "BMW528i";
+        System.out.println("model = " + model);
+        long distance = 1000000;
+        System.out.println("distance = " + distance + "K");
+        int price = 90000000;
+        System.out.println("price = " + price);
+        String company = "BMW";
+        System.out.println("company = " + company);
+        char type = 'A';
+        System.out.println("type = " + type);
+        boolean auto = true;
+        System.out.println("auto = " + auto);
+        int year = 2010;
+        System.out.println("year = " + year);
+        double gasmi = 12.5;
+        System.out.println("gasmi = " + gasmi);
+    }
+}
