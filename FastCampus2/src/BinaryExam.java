4d625ce31cb66658ad376a568c2cda4e0a4f61c1
sajieun <sajieun000721@naver.com>
2023-08-29 20:43:38 +0900

    BinaryTest,BinaryExam,FindAPI

---
+++ FastCampus2/src/BinaryExam.java
@@ -0,0 +1,13 @@
+public class BinaryExam {
+    public static void main(String[] args) {
+        // 123을 10진수, 2진수, 8진수, 16진수로 출력하시오
+        int decimal = 123;
+        System.out.println("decimal = " + decimal);
+        int binary = 0b01111011;
+        System.out.println("binary = " + binary);
+        int octal = 0173;
+        System.out.println("octal = " + octal);
+        int hexa = 0x7B;
+        System.out.println("hexa = " + hexa);
+    }
+}
