d99d3dd4b30e96c1524ee1f8aaa73a74761c2dc3
sajieun <sjieun000721@gmaill.com>
2023-10-01 20:57:54 +0900

    array , array for

---
+++ FastCampus2/src/ObjectArray/ArrayExam.java
@@ -0,0 +1,12 @@
+package ObjectArray;
+
+public class ArrayExam {
+    public static void main(String[] args) {
+        char[] c = {'A','P','P','L','E'};
+
+        for(int i=0;i<c.length;i++){
+            System.out.print((char)(c[i]+32));
+        }
+
+    }
+}
--- FastCampus2/src/ObjectArray/ArrayMake.java
+++ FastCampus2/src/ObjectArray/ArrayMake.java
@@ -2,11 +2,22 @@ package ObjectArray;
 
 import ObjectClass.Book;
 
+import java.util.Arrays;
+
 public class ArrayMake {
     public static void main(String[] args) {
         int[] a = new int[5];
-        a[0] = 34;
-        
+        a[0] = 5;
+        a[1] = 4;
+        a[2] = 3;
+        a[3] = 2;
+        a[4] = 1;
+        System.out.println(a[0]);
+        for(int i = 0;i<a.length;i++){
+            a[i] = 34;
+        }
+        System.out.println(a[0]);
+        System.out.println("_____________");
         float[] b = new float[3];
         b[0] = 293;
         System.out.println("b[0] = " + b[0]);
