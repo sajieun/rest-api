639e829de5264684179660c2070fdde4a3ae249e
sajieun <sjieun000721@gmaill.com>
2023-10-03 19:21:46 +0900

    float[], Book[], Movice[]

---
+++ FastCampus2/src/ObjectArray/ObjectArrayExam.java
@@ -0,0 +1,33 @@
+package ObjectArray;
+
+import ObjectClass.Movie;
+
+public class ObjectArrayExam {
+    public static void main(String[] args) {
+        Movie[] m = new Movie[3];
+
+        m[0] = new Movie();
+        m[0].name="about time";
+        m[0].open="2011.4.5";
+        m[0].bestmember="me";
+        m[0].genre="romance";
+        m[0].time=124;
+        m[0].age=17;
+
+        m[1] = new Movie();
+        m[1].name="about time";
+        m[1].open="2011.4.5";
+        m[1].bestmember="me";
+        m[1].genre="romance";
+        m[1].time=124;
+        m[1].age=17;
+
+        m[2] = new Movie();
+        m[2].name="about time";
+        m[2].open="2011.4.5";
+        m[2].bestmember="me";
+        m[2].genre="romance";
+        m[2].time=124;
+        m[2].age=17;
+    }
+}
