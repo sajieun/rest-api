a7afbad4c0e773d95cff4ea160e24510fd6ab530
sajieun <sjieun000721@gmaill.com>
2023-10-16 20:56:51 +0900

    chap.07 ing

---
+++ FastCampus2/src/hamsu/AssignOperTest.java
@@ -0,0 +1,16 @@
+package hamsu;
+
+public class AssignOperTest {
+    public static void main(String[] args) {
+        int a = 1;
+        a++;
+        ++a;
+        a=a+1;
+        a+=1;
+        System.out.println(a);
+//        System.out.println(a++);
+//        System.out.println(++a);
+//        System.out.println(a+=1);
+//        System.out.println(a = a+1);
+    }
+}
---
+++ FastCampus2/src/hamsu/threehang/MaxMinValue.java
@@ -0,0 +1,16 @@
+package hamsu.threehang;
+
+public class MaxMinValue {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int min = (a<b) ? a : b ;
+        int max = (a>b) ?  a : b;
+
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+
+
+    }
+}
