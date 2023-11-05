b6c5e0bcc45a581dd73fb6c18be1f01668d22d87
sajieun <sjieun000721@gmaill.com>
2023-11-06 00:29:32 +0900

    study

--- FastCampus2/src/Part3/ClassDTO/PersonTest.java
+++ FastCampus2/src/Part3/ClassDTO/PersonTest.java
@@ -9,6 +9,11 @@ public class PersonTest {
         p.age = 24;
         p.phone = "010-5555_5555";
 
+        Person p1 = new Person();
+        p.name = "jieunsa";
+        p.age = 27;
+        p.phone = "010-5555_5566";
+
         System.out.println(p.name);
         System.out.println(p.age);
         System.out.println(p.phone);
---
+++ FastCampus2/src/Part3/ClassDTO/model/CarDTO.java
@@ -0,0 +1,10 @@
+package Part3.ClassDTO.model;
+
+public class CarDTO {
+    public int carSn;
+    public String carName;
+    public int carPrice;
+    public int carOwner;
+    public int carYear;
+    public int carType;
+}
