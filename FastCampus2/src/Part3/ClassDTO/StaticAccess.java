ed8f5f4adf993d21d09c3bb8d7debdad11b7d331
sajieun <sajieun000721@naver.com>
2024-01-04 00:27:32 +0900

    빠진 부분 추가

---
+++ FastCampus2/src/Part3/ClassDTO/StaticAccess.java
@@ -0,0 +1,13 @@
+package Part3.ClassDTO;
+
+import Part3.model.MyUtil;
+
+public class StaticAccess {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = MyUtil.hap(a,b);
+    }
+}
+
+
