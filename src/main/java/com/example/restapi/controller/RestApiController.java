79d53fb06436d6183c7cc26da1962531aebf7c6f
sajieun <sajieun000721@naver.com>
2024-05-15 13:27:00 +0900

    변수 추가하여 자신이 적은 값 나오게끔 하는 실습

--- src/main/java/com/example/restapi/controller/RestApiController.java
+++ src/main/java/com/example/restapi/controller/RestApiController.java
@@ -1,4 +1,26 @@
 package com.example.restapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class RestApiController {
+
+    @GetMapping(path = "/hello")
+    public String hello(){
+        var html = "<html> <body> <h1> Hello String Boot </h1> </body> </html>";
+        return html;
+    }
+
+    @GetMapping(path = "/echo/{message}")
+    public String echo(
+            @PathVariable String message
+    ){
+        System.out.println("echo message : "+message);
+        // 대문자로 변환해서 RETURN
+        return message.toUpperCase();
+    }
 }
