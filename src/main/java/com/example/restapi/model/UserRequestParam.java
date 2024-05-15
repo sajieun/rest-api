2f921c52ad6f3b767a9c9016223e127630e37b23
sajieun <sajieun000721@naver.com>
2024-05-16 00:25:28 +0900

    post study finish

--- src/main/java/com/example/restapi/model/UserRequestParam.java
+++ src/main/java/com/example/restapi/model/UserRequestParam.java
@@ -1,5 +1,7 @@
 package com.example.restapi.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRequestParam {
-    private String name;
-    private String phone;
+    private String userName;
+    private Integer userAge;
     private String email;
 }
