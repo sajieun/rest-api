844b653c6c2a9f51f065fb1f739a5bc844d1e89c
sajieun <sajieun000721@naver.com>
2024-05-16 00:01:44 +0900

    혼자 실습

---
+++ src/main/java/com/example/restapi/model/UserRequestParam.java
@@ -0,0 +1,14 @@
+package com.example.restapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestParam {
+    private String name;
+    private String phone;
+    private String email;
+}
